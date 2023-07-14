5118ae2ad8f198aae35bdf8136df46b54bb4e1a3
David Tacite <david.tacite@gmail.com>
2023-07-14 16:25:47 +0200

    Initial Commit

---
+++ Saison/SaisonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaisonApp.swift
+//  Saison
+//
+//  Created by David Tacite on 14/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SaisonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
